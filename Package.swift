ebb748bf7ef031eac66b7944145f8920a3f3ddc8
Maurice Parker <mo@vincode.io>
2022-09-29 19:39:51 -0500

    Updated to the latest SwiftPM format

--- Package.swift
+++ Package.swift
@@ -1,5 +1,24 @@
+// swift-tools-version:5.3
+
 import PackageDescription
 
 let package = Package(
-    name: "VinXML"
+    name: "VinXML",
+	platforms: [.macOS(.v11), .iOS(.v14)],
+	products: [
+		// Products define the executables and libraries produced by a package, and make them visible to other packages.
+		.library(
+			name: "VinXML",
+			type: .dynamic,
+			targets: ["VinXML"]),
+	],
+	targets: [
+		.systemLibrary(name: "vinlibxml2", pkgConfig: "vinlibxml2"),
+		.target(
+			name: "VinXML",
+			dependencies: ["vinlibxml2"]),
+		.testTarget(
+			name: "VinXMLTests",
+			dependencies: ["VinXML"]),
+	]
 )
